240977e1d18f209ce52abd77af384320c537a986
Arpandobariya15 <work.arpan8980@gmail.com>
2024-12-30 21:00:40 +0530

    backend-routes-done

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 const express = require ('express');
 const app = express();
-const dotenv= require("dotenv").config()
-
-const cors = require("cors")
+const dotenv= require('dotenv').config()
+const connectDb = require ('./config/connectDb')
+const cors = require('cors')
+const projectRoute = require ('./routes/project')
 
 const PORT = process.env.PORT || 8080;
+connectDb();
 
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api', projectRoute);
 
 
 app.listen(PORT , (err)=>{
